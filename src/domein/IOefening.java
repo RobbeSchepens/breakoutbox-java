a85605eada499d7b618464a5255a2873ef38c868
--replace-all <'robbeschepens@hotmail.com'>
2018-04-24 15:01:54 +0200

    sd

--- src/domein/IOefening.java
+++ src/domein/IOefening.java
@@ -4,20 +4,12 @@ import java.util.List;
 import javafx.beans.property.StringProperty;
 
 public interface IOefening {
-
     String getNaam();
-
     String getAntwoord();
-
     StringProperty naamProperty();
-
     PDF getOpgave();
-
     PDF getFeedback();
-
     Vak getVak();
-
     List<Groepsbewerking> getGroepsBewerkingen();
-
     List<Doelstelling> getDoelstellingen();
 }
