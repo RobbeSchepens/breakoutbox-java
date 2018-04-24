cf0ce075321e7d6744bf2e46de17ff566192cf31
--replace-all <'robbeschepens@hotmail.com'>
2018-04-24 23:27:56 +0200

    Markup DetailPanel Oefening

--- src/gui/DetailPanelOefeningController.java
+++ src/gui/DetailPanelOefeningController.java
@@ -1,16 +1,15 @@
 package gui;
 
-import domein.Doelstelling;
 import domein.DomeinController;
-import domein.Groepsbewerking;
+import domein.IOefening;
 import domein.OefeningObserver;
 import domein.Vak;
 import java.io.IOException;
-import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -19,9 +18,22 @@ public class DetailPanelOefeningController extends VBox implements OefeningObser
 
     private DomeinController dc;
     @FXML private Label lblTitleRight;
-    @FXML private Label lblNaam;
     @FXML private TextField txfNaam;
     @FXML private Button btnAdd;
+    @FXML private TextField txfAntwoord;
+    @FXML private ChoiceBox<Vak> ddlVak;
+    @FXML private Button btnGroepsbewerkingen;
+    @FXML private Label lblGroepsbewerkingenCount;
+    @FXML private Button btnDoelstellingen;
+    @FXML private Label lblDoelstellingenCount;
+    @FXML private Button btnEdit;
+    @FXML private Button btnNieuweOefening;
+    @FXML private Button btnOpenFeedback;
+    @FXML private Button btnFileFeedback;
+    @FXML private Label lblFeedback;
+    @FXML private Button btnOpenOpgave;
+    @FXML private Button btnFileOpgave;
+    @FXML private Label lblOpgave;
 
     public DetailPanelOefeningController(DomeinController dcon) {
         FXMLLoader loader
@@ -35,17 +47,82 @@ public class DetailPanelOefeningController extends VBox implements OefeningObser
         }
         
         this.dc = dcon;
+        
+        // Init nieuwe oefening
+        ddlVak.setItems(dc.geefVakken());
+        btnNieuweOefening.setVisible(false);
+        btnEdit.setManaged(false);
+        btnEdit.setVisible(false);
+    }
+
+    @Override
+    public void update(IOefening o) {
+        btnNieuweOefening.setVisible(true);
+        btnAdd.setManaged(false);
+        btnAdd.setVisible(false);
+        btnEdit.setManaged(true);
+        btnEdit.setVisible(true);
+        
+        txfNaam.setText(o.getNaam());
+        txfAntwoord.setText(o.getAntwoord());
+        ddlVak.setItems(dc.geefVakken());
+        ddlVak.getSelectionModel().select(o.getVak());
+        lblOpgave.setText("naam.pdf");
+        lblFeedback.setText("naam.pdf");
+        lblGroepsbewerkingenCount.setText(o.getGroepsBewerkingen().size() + " bewerkingen geselecteerd");
+        lblDoelstellingenCount.setText(o.getDoelstellingen().size() + " doelstellingen geselecteerd");
+    }
+
+    @FXML
+    private void btnNieuweOefeningOnAction(ActionEvent event) {
+        btnNieuweOefening.setVisible(false);
+        btnAdd.setManaged(true);
+        btnAdd.setVisible(true);
+        btnEdit.setManaged(false);
+        btnEdit.setVisible(false);
+        
+        txfNaam.setText("");
+        txfAntwoord.setText("");
+        ddlVak.setItems(dc.geefVakken());
+        ddlVak.getSelectionModel().clearSelection();
+        lblOpgave.setText("");
+        lblFeedback.setText("");
+        lblGroepsbewerkingenCount.setText("0 bewerkingen geselecteerd");
+        lblDoelstellingenCount.setText("0 doelstellingen geselecteerd");
+    }
+
+    @FXML
+    private void btnOpenOpgaveOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnFileOpgaveOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnOpenFeedbackOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnFileFeedbackOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnGroepsbewerkingenOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnDoelstellingenOnAction(ActionEvent event) {
     }
 
     @FXML
     private void btnAddOnAction(ActionEvent event) {
+        //dc.voegNieuweOefeningToe(txfNaam.getText(), txfAntwoord.getText(), opgave, feedback, vak, groepsbewerkingen, doelstelling);
     }
 
-    @Override
-    public void update(String naam, String antwoord, Vak vak, List<Groepsbewerking> groepsbewerkingen, List<Doelstelling> doelstellingen) {
-        System.out.println(naam);
-        System.out.println(antwoord);
-        txfNaam.setText(naam);
+    @FXML
+    private void btnEditOnAction(ActionEvent event) {
+        System.out.println("test");
     }
     
 }
