7523af749885ee38d691e261c989a9a6446643ba
DaanDedecker <daan.dedecker.y0420@student.hogent.be>
2018-04-29 19:51:27 +0200

    adding box classes, gui

--- src/gui/OverzichtPanelBoxController.java
+++ src/gui/OverzichtPanelBoxController.java
@@ -6,11 +6,48 @@
 package gui;
 
 import domein.BoxController;
+import domein.IBox;
+import java.util.Observable;
+import javafx.event.ActionEvent;
 
 /**
  *
  * @author Daan
  */
-public class OverzichtPanelBoxController extends OverzichtPanelController<Ibox, BoxController> {
+public class OverzichtPanelBoxController extends OverzichtPanelController<IBox, BoxController> {
+
+    public OverzichtPanelBoxController() {
+        super(null);
+    }
+
+    @Override
+    void btnDeleteSelectedOnAction(ActionEvent event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    <T> void implementTableviewListener(T newValue) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    void renderContent() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    void filter(String newValue) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    void initNieuw() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Observable o, Object o1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
