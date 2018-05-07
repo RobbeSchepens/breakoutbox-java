f4fcd36e0e11a91b48d1f08716a27d4ecf10f4bf
'Robbe Schepens' <'robbeschepens@hotmail.com'>
2018-05-07 23:59:35 +0200

    Improve error message opening pdf

--- src/gui/OefeningDetailPanelController.java
+++ src/gui/OefeningDetailPanelController.java
@@ -158,7 +158,7 @@ public class OefeningDetailPanelController extends VBox implements OefeningObser
                 lblError.setText("Openen van de PDF is niet ondersteund.");
             }
         } catch (IOException ioe) {
-            System.out.println(ioe);
+            lblError.setText("Er is iets fout gelopen bij het openen van het bestand.");
         }
     }
 
@@ -166,7 +166,6 @@ public class OefeningDetailPanelController extends VBox implements OefeningObser
     private void btnFileOpgaveOnAction(ActionEvent event) {
         fileOpgave = fileChooserOpgave.showOpenDialog((Stage)(this.getScene().getWindow()));
         if (fileOpgave != null) {
-            System.out.println("hier");
             fileOpgaveNaam = fileOpgave.getName();
             lblOpgave.setText(fileOpgaveNaam);
             btnOpenOpgave.setDisable(false);
@@ -183,7 +182,7 @@ public class OefeningDetailPanelController extends VBox implements OefeningObser
                 lblError.setText("Openen van de PDF is niet ondersteund.");
             }
         } catch (IOException ioe) {
-            System.out.println(ioe);
+            lblError.setText("Er is iets fout gelopen bij het openen van het bestand.");
         }
     }
 
